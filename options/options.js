b6d11e7d0899a4873897c3319d51a1260ea6bac0
Crowdbotics <team@crowdbotics.com>
2022-05-17 16:30:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "heartland_charter_s_34954",
+  url: "https://heartland_charter_s_34954.botics.co",
+  api: "https://heartland_charter_s_34954.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
