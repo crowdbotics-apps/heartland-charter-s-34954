651d2b4bfde8cb9186c4eed2f9b3d939d6ebe457
Nziranziza <nziranzizadaniel@gmail.com>
2022-07-14 17:52:52 +0200

    Onboarding screens - Update the onboarding modules - Add sample onboarding slides - Redirect to login

--- modules/onboarding/slides.js
+++ modules/onboarding/slides.js
@@ -1,29 +1,22 @@
+import { faker } from '@faker-js/faker';
+
 export const slides = [
   {
     key: 'one',
-    title: 'Title 1',
-    text: 'Description.\nSay something cool',
     image: {
-      uri:
-        'https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png',
-    },
-    backgroundColor: '#59b2ab',
+      uri: faker.image.people(undefined, undefined, true),
+    }
   },
   {
     key: 'two',
-    title: 'Title 2',
-    text: 'Other cool stuff',
     image: {
-      uri:
-        'https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png',
-    },
-    backgroundColor: '#febe29',
+      uri: faker.image.people(undefined, undefined, true),
+    }
   },
-  // {
-  //   key: 'three',
-  //   title: 'Rocket guy',
-  //   text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
-  //   image: require('./assets/3.jpg'),
-  //   backgroundColor: '#22bcb5',
-  // }
+  {
+    key: 'three',
+    image: {
+      uri: faker.image.people(undefined, undefined, true),
+    }
+  },
 ];
